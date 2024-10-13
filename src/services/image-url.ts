5f0f4a4547909b9290621e53f8ddfe0a7bf618c3
Shibam Kumar Patra <shibampatra01@gmail.com>
2024-10-13 18:09:43 +0530

    fix layout issue and remove console.log

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,6 +1,5 @@
 const getCroppedImageUrl = (url: string) => {
   const index = url.indexOf("media/") + "media/".length;
-  console.log(index);
   return url.slice(0, index) + "crop/600/400/" + url.slice(index);
 };
 
